72189edb643eb8ed8fee72819a4da402c7ba6e7a
Nico Auerswald <auersw.nico@gmail.com>
2026-06-02 14:21:16 +0200

    implement naive line drawing

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    for (float t = 0.; t < 1.; t += .02) {
+        int x = std::round( ax + (bx - ax) * t);
+        int y = std::round( ay + (by - ay) * t);
+
+        framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -16,9 +25,14 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    framebuffer.set(cx, cy, red);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
